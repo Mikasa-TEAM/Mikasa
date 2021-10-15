c2d588f3b85ce9282c2e007a17a5e9626ad0d7bf
Mikasa <92240907+Mikasa-TEAM@users.noreply.github.com>
2021-10-15 04:08:47 +0300

    Add files via upload

--- Mikasa.lua
+++ Mikasa.lua
@@ -4453,12 +4453,12 @@ end
 if text == "السورس" or text == "سورس" then
 Text = [[
 Welcome To Source
-𓄇[˛ 𝙢𝓲𝙠𝙨𝙖ꪖ .](httpsك://t.me/TeAmMikasa)
+[˛ 𝙢𝓲𝙠𝙨𝙖ꪖ .](httpsك://t.me/TeAmMikasa)
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '˛ ꪑ𝓲𝙠ꪖ𝘴𝙖 𝙩ꫀꪖ𝙢 .',url="https://t.me/TeAmMikasa"}},
-{{text = '˛ ᦔꫀ𝙫 𝙨ꪊ𝙧𝘴 .',url="t.me/DODDD"},{text = '˛ 𝓽᭙𝘴 bꪮ𝓽 .',url="https://t.me/Yv4bot"}},{{text= '˛ ꪑ𝙮 ᥴ𝙝ꪖꪀ𝙣ꫀꪶ .',url="https://t.me/DoDDDD"}},
+{{text = '˛ ᦔꫀ𝙫 𝙨ꪊ𝙧𝘴 .',url="t.me/DODDD"},{text = '˛ 𝓽᭙𝘴 bꪮ𝓽 .',url="https://t.me/Yv4bot"}},{{text= '˛ ꪑ𝙮 ᥴ𝙝ꪖꪀ𝙣ꫀꪶ .',url="https://t.me/DoDDDD"}}
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/TeAmMikasa&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
